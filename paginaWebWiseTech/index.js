65052c5beef35d257b8a37cc810672efd9250efd
franck2014 <fjtsystems@gmail.com>
2021-11-13 19:59:18 -0300

    se agrego fire y se anexo formulario con firestore, se bajo 300 pixeles texto, se elimino reunion, se agrego mensaje de mensaje enviado

---
+++ paginaWebWiseTech/index.js
@@ -0,0 +1,44 @@
+
+const db = firebase.firestore();
+
+const todoForm = document.getElementById('todo_form');
+
+const create = (name,
+    datecreated,
+    email,telephone,
+   message) => {
+    db.collection('contacto').doc().set({
+        name,
+        datecreated,
+        email,
+    telephone,
+       message
+    })
+}
+
+todoForm.addEventListener('submit', async e => {
+    escribir2("ok")
+    e.preventDefault();
+    const name = todoForm['todo_name'].value;
+    const datecreated= new Date();
+    const email = todoForm['todo_url'].value;
+    const telephone = todoForm['todo_phone'].value;
+    const message = todoForm['todo_description'].value;
+
+    await create(name,
+        datecreated,
+        email,
+        telephone,
+       message); 
+       
+    todoForm.reset(); 
+    
+});
+
+function escribir2(algo) {
+    if(algo=="ok"){
+       document.getElementById('cont3').innerHTML='Mensaje enviado correctamente ✔'; 
+    }
+     
+    }
+    
